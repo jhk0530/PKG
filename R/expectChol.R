f90326504a75dd52442fc8a6b95803a32fd45f73
Jinhwan Kim <hwanistic@gmail.com>
2024-06-13 21:47:08 +0900

    Update expectChol.R

--- R/expectChol.R
+++ R/expectChol.R
@@ -1,8 +1,14 @@
 #' @title predict cholesterol with weight and smoke information
 #' @export
 expectChol <- function(weight, smoke){
+  smoke <- validSmoke(smoke)
   input <- data.frame(G1E_WGHT = weight, Q_SMK_YN = smoke)
   chol <- predict(model, input)
   chol <- round(chol, 1)
   return(unname(chol))
+}
+
+validSmoke <- function(smoke){
+  if(!is.factor(smoke)) return(factor(smoke))
+  return(smoke)
 }
